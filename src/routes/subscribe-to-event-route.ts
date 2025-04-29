7c4e9853191741bbd8c9a9e601997a01cd304c2b
Marcelo Jacubowski <marcelosj99@gmail.com>
2025-04-28 22:37:48 -0300

    feat: aula 1

---
+++ src/routes/subscribe-to-event-route.ts
@@ -0,0 +1,33 @@
+import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod'
+import z from 'zod'
+
+export const subscribeToEventRoute: FastifyPluginAsyncZod = async app => {
+  app.post(
+    '/subscriptions',
+    {
+      schema: {
+        summary: 'subscribe someone',
+        tags: ['subscription'],
+        description: 'very long desc of describe',
+        body: z.object({
+          name: z.string(),
+          email: z.string().email(),
+        }),
+        response: {
+          201: z.object({
+            name: z.string(),
+            email: z.string(),
+          }),
+        },
+      },
+    },
+    async (request, reply) => {
+      const { name, email } = request.body
+
+      return reply.status(201).send({
+        name,
+        email,
+      })
+    }
+  )
+}
